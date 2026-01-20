ec5d08a15bc643937d0285f22abfe2eafbc14761
Sand <116615072+Sarawich@users.noreply.github.com>
2026-01-20 14:13:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    strictPort: false,
+    allowedHosts: ['.onrender.com']
+  }
 })
